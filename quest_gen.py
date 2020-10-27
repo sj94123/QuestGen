3c7967e5e5b064d203851872c54343507bfe0a08
Austin Shafer <ashafer@badland.io>
2020-10-27 18:54:23 +0000

    Make first (bad) working protoype

--- quest_gen.py
+++ quest_gen.py
@@ -2,24 +2,27 @@
 #
 # Austin Shafer - 2020
 
+from nltk.tokenize import sent_tokenize
 import tracery
 import json
-import gpt_2_simple as gpt2
+from gpt2_client import GPT2Client
 import os
 import nltk.data
 
 # we need this to split things into sentences
-tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+nltk.download('punkt')
 
 # download the gpt-2 model
 # (this is from the gpt-2-simple tutorial)
 model_name = "124M"
-if not os.path.isdir(os.path.join("models", model_name)):
+model_path = os.path.join("models", model_name)
+if not os.path.isdir(model_path):
 	print(f"Downloading {model_name} model...")
 	gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/*/
 
 # start gpt-2
-sess = gpt2.start_tf_sess()
+gpt2 = GPT2Client('345M') # This could also be `345M`, `774M`, or `1558M`. Rename `save_dir` to anything.
+gpt2.load_model(force_download=False) # Use cached versions if available.
 # TODO: finetune?
 
 # open our tracery grammar
@@ -35,15 +38,16 @@ with open('quests.json', 'r') as f:
         # generate a procedural quest using tracery
         proc_generated = grammar.flatten("#origin#")
         # split it into sentences
-        sentences = tokenizer.tokenize(proc_generated)
+        sentences = sent_tokenize(proc_generated)
 
         final_quest = ""
         # generate one gpt-2 sentence for each sentence in base
         # (except the last sentence)
         for i in range(0, len(sentences) - 1):
             sent = sentences[i]
-            gpt_sent = gpt2.generate(sess)
-            final_quest = final_quest + sent + gpt_sent
+            gpt_raw = gpt2.generate_batch_from_prompts([sent])[0]
+            gpt_sent = sent_tokenize(gpt_raw)[0]
+            final_quest = "{} {} {}".format(final_quest, sent, gpt_sent)
 
-        print(final_quest)
-        print('=========== =====')
+        print(final_quest + sentences[len(sentences) - 1])
+        print('================')
